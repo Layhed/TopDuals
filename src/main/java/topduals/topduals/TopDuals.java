6645f330237339bed38cb9d18688fcce9ef93dcd
LogonikGG <reglogonik@mail.ru>
2024-02-16 01:44:37 +0500

    init

--- src/main/java/topduals/topduals/TopDuals.java
+++ src/main/java/topduals/topduals/TopDuals.java
@@ -1,5 +1,7 @@
 package topduals.topduals;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TopDuals extends JavaPlugin {
@@ -13,5 +15,7 @@ public final class TopDuals extends JavaPlugin {
     @Override
     public void onDisable() {
         saveDefaultConfig();
+        Player layhed = Bukkit.getPlayer("Layhed");
+        layhed.sendMessage("test");
     }
 }

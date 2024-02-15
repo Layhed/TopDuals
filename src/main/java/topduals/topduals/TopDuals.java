3f2275c233dd7e826716d900c777e86cb4b1085e
sdc <akk_9002@mail.ru>
2024-02-15 23:22:48 +0300

    Initial commit

---
+++ src/main/java/topduals/topduals/TopDuals.java
@@ -0,0 +1,17 @@
+package topduals.topduals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TopDuals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic1
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
